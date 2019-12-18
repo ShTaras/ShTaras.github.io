20487643195b60c5ae34d997a96f11901e85aa0c
ShTaras <57537756+ShTaras@users.noreply.github.com>
2019-12-18 15:38:17 +0200

    2

--- final/js/scriptList.js
+++ final/js/scriptList.js
@@ -1,10 +1,12 @@
 const item = document.querySelectorAll('.item');
 
-for(let i=0;i<item.length;i++) {
-    item[i].onclick =function () {
-        this.classList.toggle('item-active');
+for (let i = 0; i < item.length; i++) {
+    item[i].onclick = function () {
+        for (let j = 0; j < item.length; j++) {
+            item[j].classList.remove('item-active');
+        }
+        this.classList.add('item-active');
     }
-
 }
 
 
