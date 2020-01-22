a045b68bd1823ef398725548d8533404b562c19a
ShTaras <57537756+ShTaras@users.noreply.github.com>
2020-01-22 15:29:28 +0200

    2

--- js/1hw/js/script.js
+++ js/1hw/js/script.js
@@ -1,84 +1,168 @@
+let taskTemp;
 let todoList = [];
-let temp;
+
+let inputTask = document.getElementById('taskText');
+let btnAdd = document.getElementById('btnAdd');
+btnAdd.addEventListener("click", addTask);
 
 
 if (localStorage.getItem('todo') != undefined) {
+    loadTasks();
+    print();
+}
+
+
+function loadTasks() {
     todoList = JSON.parse(localStorage.getItem('todo'));
-
-    printTasks();
 }
-document.getElementById('btnAdd').onclick = function () {
 
-    temp = {};
-    let d = document.getElementById('taskText').value;
+function addTask() {
+    initTask();
+    save();
+    print();
+}
+
+function initDate() {
     let date = new Date();
-    let day = date.getDay(),
-        month = date.getMonth() + 1,
-        hours = date.getHours(),
-        minutes = date.getMinutes();
+    let taskDate = {};
+    taskDate.day = date.getDay();
+    taskDate.month = date.getMonth() + 1;
+    taskDate.year = date.getFullYear();
+    taskDate.hours = date.getHours();
+    taskDate.minutes = date.getMinutes();
 
-    if (day < 10) day = '0' + day;
-    if (month < 10) month = '0' + month;
-    if (hours < 10) hours = '0' + hours;
-    if (minutes < 10) minutes = '0' + minutes;
+    if (taskDate.day < 10) taskDate.day = '0' + taskDate.day;
+    if (taskDate.month < 10) taskDate.month = '0' + taskDate.month;
+    if (taskDate.hours < 10) taskDate.hours = '0' + taskDate.hours;
+    if (taskDate.minutes < 10) taskDate.minutes = '0' + taskDate.minutes;
 
-    temp.dateInfo = day + '.' + month + '.' + date.getFullYear();
-    temp.timeInfo = hours + ':' + minutes;
-    temp.importance = 1;
-    temp.taskText = d;
-    temp.check = false;
-    temp.taskID = `f${(+date).toString(16)}`;
-
-    let i = todoList.length;
-    todoList[i] = temp;
-
-    localStorage.setItem('todo', JSON.stringify(todoList));
-    printTasks();
-    console.log(todoList);
-
+    return taskDate;
 }
 
+function initTask() {
 
-function printTasks() {
+
+    let taskDate = initDate();
+    taskTemp = {};
+    taskTemp.dateInfo = taskDate.day + '.' + taskDate.month + '.' + taskDate.year;
+    taskTemp.timeInfo = taskDate.hours + ':' + taskDate.minutes;
+    taskTemp.importance = 1;
+    taskTemp.taskText = inputTask.value;
+    taskTemp.check = false;
+    taskTemp.taskID = createID();
+    inputTask.value = "";
+}
+
+function save() {
+    todoList[todoList.length] = taskTemp;
+    localStorage.setItem('todo', JSON.stringify(todoList));
+}
+
+function createID() {
+    let date = new Date();
+    let id = `f${(+date).toString(16)}`;
+    return id;
+}
+
+function print() {
     let oldTasks = document.querySelectorAll('.task');
     for (let i = 1; i < oldTasks.length; i++) {
         oldTasks[i].remove();
     }
-    for (let pair of todoList.entries()) {
-
+    for (let i = 0; i < todoList.length; i++) {
         let newTask = document.querySelector('.task-empty').cloneNode(true);
-
-        for (let key in pair) {
-            newTask.querySelector('.task__date').innerHTML = pair[key].dateInfo
-            newTask.querySelector('.task__time').innerHTML = pair[key].timeInfo;
-            newTask.querySelector('.task__importance').innerHTML = pair[key].importance;
-            newTask.querySelector('.task__text').innerHTML = pair[key].taskText;
-            newTask.id = pair[key].taskID;
-            if(pair[key].check===true){
+        for (let key in todoList[i]) {
+            newTask.querySelector('.task__date').innerHTML = todoList[i].dateInfo
+            newTask.querySelector('.task__time').innerHTML = todoList[i].timeInfo;
+            newTask.querySelector('.task__importance').innerHTML = todoList[i].importance;
+            newTask.querySelector('.task__text').innerHTML = todoList[i].taskText;
+            newTask.id = todoList[i].taskID;
+            if (todoList[i].check === true) {
                 newTask.classList.add('task-complete');
             }
         }
-
         newTask.classList.remove('task-empty');
         document.getElementById('todoList').appendChild(newTask);
-        newTask.addEventListener("click", changeStatus);
+        //newTask.addEventListener("click", changeStatus);
 
+        addBtnEvent(newTask);
     }
 
 }
 
+function addBtnEvent(newTask) {
+    newTask.addEventListener("click", changeStatus);
+    newTask.addEventListener("click", showDltMsg);
+}
+
+function deleteTask() {
+
+    let target = event.target;
+    if (target.className === 'task__btnDelete') {
+        for (let i = 0; i < todoList.length; i++) {
+            if (todoList[i].taskID === this.id) {
+                todoList.splice(i, 1);
+            }
+        }
+    }
+    localStorage.setItem('todo', JSON.stringify(todoList));
+    print();
+}
+
+function showDltMsg() {
+    let DltMsg = {
+        todo: document.getElementById('todoList'),
+        window: document.createElement('div'),
+        btnYes: document.createElement('button'),
+        btnNo: document.createElement('button'),
+        close: function () {
+            DltMsg.todo.style.opacity = '1';
+            DltMsg.todo.style.pointerEvents = 'All';
+            document.body.removeChild(DltMsg.window);
+        }
+    }
+
+
+    DltMsg.window.className = 'deleteWindow';
+    DltMsg.btnNo.className = 'deleteWindow__btnYn';
+    DltMsg.btnNo.innerHTML = 'No';
+
+    DltMsg.btnYes.className = 'deleteWindow__btnYn';
+    DltMsg.btnYes.innerHTML = 'Yes';
+
+    DltMsg.todo.style.opacity = '0.5';
+    DltMsg.todo.style.pointerEvents = 'none';
+    document.body.appendChild(DltMsg.window);
+    DltMsg.window.innerHTML = '<p class="deleteWindow__notice">Delete item?</p>';
+    DltMsg.window.appendChild(DltMsg.btnNo);
+    DltMsg.window.appendChild(DltMsg.btnYes);
+
+
+    DltMsg.btnNo.addEventListener('click', DltMsg.close);
+    DltMsg.btnYes.addEventListener('click', function () {
+        deleteTask();
+        DltMsg.close();
+    });
+
+
+}
+
+// function closeDltMsg(DltMsg) {
+//
+//
+// }
 
 function changeStatus() {
 
     let target = event.target;
     if (target.className === 'task__btnCheck') {
-        for(let i = 0 ; i< todoList.length ; i++){
-            if(todoList[i].taskID=== this.id){
-                todoList[i].check= true;
+        for (let i = 0; i < todoList.length; i++) {
+            if (todoList[i].taskID === this.id) {
+                todoList[i].check = true;
             }
         }
     }
     localStorage.setItem('todo', JSON.stringify(todoList));
-    printTasks();
+    print();
 }
 
